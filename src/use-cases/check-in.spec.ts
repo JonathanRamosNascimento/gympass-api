a83dda0480298c700c2efc19bec17862068b1e32
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2023-05-22 23:10:59 -0300

    criando a função para retornar a disância entre dois pontos e finalizando o useCase de checkIn somente a 100 metros da academia

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -19,8 +19,8 @@ describe('Check-in Use Case', () => {
       title: 'JavaScript Gym',
       description: '',
       phone: '',
-      latitude: new Decimal(0),
-      longitude: new Decimal(0),
+      latitude: new Decimal(-16.3200221),
+      longitude: new Decimal(-48.9303596),
     })
 
     vi.useFakeTimers()
@@ -81,5 +81,25 @@ describe('Check-in Use Case', () => {
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in on distant gym', async () => {
+    gymsRepository.items.push({
+      id: 'gym-02',
+      title: 'JavaScript Gym',
+      description: '',
+      phone: '',
+      latitude: new Decimal(-16.3049266),
+      longitude: new Decimal(-48.8948458),
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-02',
+        userId: 'user-01',
+        userLatitude: -16.3200221,
+        userLongitude: -48.9303596,
+      }),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
