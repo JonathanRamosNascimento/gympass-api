7c645ee2fb7d5a44615871553b6dafe6b8bc7b63
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2023-05-30 22:45:45 -0300

    criando o use case para historico de checkins e o teste

---
+++ src/use-cases/fetch-user-check-ins-history.ts
@@ -0,0 +1,24 @@
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+import { CheckIn } from '@prisma/client'
+
+interface FetchUserCheckInsHistoryUseCaseRequest {
+  userId: string
+}
+
+interface FetchUserCheckInsHistoryUseCaseResponse {
+  checkIns: CheckIn[]
+}
+
+export class FetchUserCheckInsHistoryUseCase {
+  constructor(private checkInRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+  }: FetchUserCheckInsHistoryUseCaseRequest): Promise<FetchUserCheckInsHistoryUseCaseResponse> {
+    const checkIns = await this.checkInRepository.findManyByUserId(userId)
+
+    return {
+      checkIns,
+    }
+  }
+}
