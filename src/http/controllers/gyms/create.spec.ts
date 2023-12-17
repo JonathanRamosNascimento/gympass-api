1c01b385a4db8c0eee243a9329ebacbcc6a6999c
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2023-12-17 16:51:18 -0300

    teste e2e de criação de gym

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,31 @@
+import { app } from '@/app'
+import { CreateAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Create Gym (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a gym', async () => {
+    const { token } = await CreateAndAuthenticateUser(app)
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Javascript Gym',
+        description: 'Some description',
+        phone: '62999999999',
+        latitude: -16.3200221,
+        longitude: -48.9303596,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
