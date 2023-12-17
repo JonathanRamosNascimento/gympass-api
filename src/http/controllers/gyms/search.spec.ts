0bc72002245f78b75711866af01b425b8c5121fa
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2023-12-17 17:00:27 -0300

    Teste e2e buscarndo academias

---
+++ src/http/controllers/gyms/search.spec.ts
@@ -0,0 +1,56 @@
+import { app } from '@/app'
+import { CreateAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Search Gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a gym', async () => {
+    const { token } = await CreateAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Javascript Gym',
+        description: 'Some description',
+        phone: '62999999999',
+        latitude: -16.3200221,
+        longitude: -48.9303596,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Angular Gym',
+        description: 'Some description',
+        phone: '62999999999',
+        latitude: -16.3200221,
+        longitude: -48.9303596,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/search')
+      .query({
+        q: 'Javascript',
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'Javascript Gym',
+      }),
+    ])
+  })
+})
